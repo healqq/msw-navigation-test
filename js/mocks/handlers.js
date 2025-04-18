4bd4099a640d72a5ebec82b014e6fbc8e1590a95
healqq <nyashin92@gmail.com>
2025-04-18 10:46:42 +0200

    small adjustments

--- js/mocks/handlers.js
+++ js/mocks/handlers.js
@@ -3,7 +3,7 @@ import { http, HttpResponse } from "msw";
 
 export const handlers = [
   // Intercept "GET https://example.com/user" requests...
-  http.get("http://localhost:5173/foo", () => {
+  http.get("/foo", () => {
     // ...and respond to them using this JSON response.
     return HttpResponse.html("<html><body>foo</body></html>");
   }),
